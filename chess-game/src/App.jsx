cc1851ab28f7f65e2383d2e6df8cb64c9646e048
Leprekus <leprekus@yahoo.com>
2022-12-25 22:38:49 -0800

    toggle possible moves

--- chess-game/src/App.jsx
+++ chess-game/src/App.jsx
@@ -1,10 +1,15 @@
-import { useRef, useMemo, useState } from 'react'
+import { useEffect, useCallback, useState, useMemo } from 'react'
 import Draggable from 'react-draggable'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Pawn from './components/Pawn'
 function App() {
   const [updateBoard, setUpdateBoard] = useState(false)
+  useMemo(() => {
+    if(updateBoard) {
+      setUpdateBoard(!updateBoard)
+    }
+  }, [updateBoard])  
   const [board, setBoard] = useState([
     [{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false}],
     [{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false}],
@@ -15,20 +20,9 @@ function App() {
     [{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false}],
     [{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false},{className: false}],
   ])
-  useMemo(() => {
-      if(updateBoard) {
-        setUpdateBoard(!updateBoard)
-      }
 
-  }, [updateBoard])
-  const showMoves = () => {
-    setBoard((prevState) => {
-      prevState[2 + 1][5].className = 'active'
-      prevState[2 + 2][5].className = 'active'
-      return prevState
-    })
-    setUpdateBoard(!updateBoard)
- }
+
+ 
   // moves 
   // pawn = board[0 + 1][0] || board[0 + 2][0]
   // tower = {
@@ -51,12 +45,12 @@ function App() {
     <div className="App">
     <div className="board">
       {
-        board.map((row, x) =>
-        <div key={x} className='row'>{
+        board.map((row, y) =>
+        <div key={y} className='row'>{
 
-          row.map((tile, y) =>
-          <span key={`${coords[x]}-${y}`} style={{ backgroundColor: (x + y) % 2 === 0 ? 'tomato' : 'black' }} className={`tile ${board[x][y].className}`} onClick={showMoves}>
-            { `${coords[x]}${y}` === 'b4' ? <Pawn board={board} posX={x} posY={y} setBoard={setBoard}/> : ` ${coords[x]}${y} ` }
+          row.map((tile, x) =>
+          <span key={`${coords[x]}-${y}`} style={{ backgroundColor: (x + y) % 2 === 0 ? 'tomato' : 'black' }} className={`tile ${board[x][y].className}`}>
+            { `${coords[x]}${y}` === 'f1' ? <Pawn board={board} posX={x} posY={y} setBoard={setBoard} setUpdateBoard={setUpdateBoard}/> : ` ${coords[x]}${y} ` }
           </span>)
 
         }</div>)
--- chess-game/src/components/Pawn.jsx
+++ chess-game/src/components/Pawn.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
 import Draggable from "react-draggable"
 import blackPawn from '../assets/black_pawn.png'
-const Pawn = () => {
+const Pawn = ({ board, posX, posY, setBoard, setUpdateBoard}) => {
+    const [prevX, setPrevX] = useState(null)//need to be global
+    const [prevY, setPrevY] = useState(null)//need to be global
+    const handleMoves = (x, y, classNameState) => {
+        setBoard(prevState =>{ 
+            prevState[posX][posY + 1].className = classNameState
+            prevState[posX][posY + 2].className = classNameState
+            return prevState 
+            })
+            setPrevX(x)
+            setPrevY(y)
+        setUpdateBoard(prevState => !prevState)    
+    }
+   const highlightMoves = () => {
+    prevX === null && prevY === null ?
+    handleMoves(posX, posY, 'active') :
+    handleMoves(null, null, false)
+   }
     return (
-        <div>
+        <div onClick={highlightMoves}>
             <Draggable>
                 <div className="piece">
                 </div>
